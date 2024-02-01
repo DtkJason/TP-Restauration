7dabd6a6503870748e00f09b300c6f5e45f6f5d4
JordanRND <r.jordan1902@gmail.com>
2024-02-01 12:24:52 +0100

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 import "./index.css";
 
 export const Header = () => {
   return (
     <header className="header-container">
-      <img href="" alt="Logo" />
       <Link to="/" className="navbar">
         Accueil
       </Link>
+      <img src={logo} alt="Logo" className="logo" />
       <Link to="/Menu" className="navbar">
         Menu
       </Link>
