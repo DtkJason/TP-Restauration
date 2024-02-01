4aacc589d5c40376076346e75e138f07705ce5d1
JordanRND <r.jordan1902@gmail.com>
2024-02-01 12:18:35 +0100

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 
+import "./index.css";
+
 export const Header = () => {
   return (
-    <header>
-      <Link to="/">Accueil</Link>
-      <Link to="/Menu">Menu</Link>
+    <header className="header-container">
+      <img href="" alt="Logo" />
+      <Link to="/" className="navbar">
+        Accueil
+      </Link>
+      <Link to="/Menu" className="navbar">
+        Menu
+      </Link>
     </header>
   );
 };
