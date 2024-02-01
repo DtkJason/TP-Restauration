15a03ada7caa9ec1ee9c9c8ba1a8f5547d70b200
JordanRND <r.jordan1902@gmail.com>
2024-02-01 16:01:11 +0100

    update map

---
+++ src/components/Map/index.js
@@ -0,0 +1,18 @@
+import Iframe from "react-iframe";
+import "./index.css";
+
+export const Map = () => {
+  return (
+    <div className="map-container">
+      <h3>Où nous trouver ?</h3>
+      <div className="map">
+        <Iframe
+          title="map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.5036260294855!2d2.3860583156740183!3d48.84860607928655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6720ccc2ca7b1%3A0x45528eb90c108f61!2sIPSSI%20Paris!5e0!3m2!1sfr!2sfr!4v1678201217034!5m2!1sfr!2sfr"
+          allowFullScreen=""
+          loading="lazy"
+        ></Iframe>
+      </div>
+    </div>
+  );
+};
--- src/pages/Accueil/index.js
+++ src/pages/Accueil/index.js
@@ -1,15 +1,20 @@
 import { Banner } from "../../components/Banner";
+import { Map } from "../../components/Map";
 import img from "../../assets/burger.png";
+import "./index.css";
 
 const Accueil = () => {
   return (
-    <Banner
-      title="Titre"
-      content="Ceci est une description"
-      imagePath={img}
-      imagePosition="right"
-      textButton="Voir Carte"
-    />
+    <div className="accueil-container">
+      <Banner
+        title="Titre"
+        content="Ceci est une description"
+        imagePath={img}
+        imagePosition="right"
+        textButton="Voir Carte"
+      />
+      <Map />
+    </div>
   );
 };
 
