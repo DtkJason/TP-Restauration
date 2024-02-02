e7c2d55b76efb32a71e67d453381084d321d9558
DtkJason <kinvij0.@gmail.com>
2024-02-02 11:47:31 +0100

    img and card modif

--- src/components/Card/CardPrimary/index.js
+++ src/components/Card/CardPrimary/index.js
@@ -1,5 +1,8 @@
 import "./index.css";
+import Button from "../../Button";
+import { useState } from "react";
 export const Primary = ({ title, label, details, Background }) => {
+  const [texto, setColor] = useState("");
   return (
     <div className="card1">
       <div className="card-header">
@@ -9,8 +12,20 @@ export const Primary = ({ title, label, details, Background }) => {
         <h2 className="card-title">{title}</h2>
         <p className="detail">{label}</p>
         <div className="details">
-          <p>{details}</p>
+          <p>{texto}</p>
         </div>
+
+        <button
+          className="buton"
+          type="button"
+          onClick={() => setColor(details)}
+        >
+          <Button.Primary label="details"></Button.Primary>
+        </button>
+
+        <button className="buton" type="button" onClick={() => setColor("")}>
+          <Button.Primary label="Commander"></Button.Primary>
+        </button>
       </div>
     </div>
   );
